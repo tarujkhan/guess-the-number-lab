e7ba3eeea56bb9b3524e05a6c7de393e60bd64b8
tarujkhan <tarujkhan@yahoo.com>
2022-06-22 17:47:52 -0500

    Still working on the project and adding some logic.

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    prevGuess: [],
+    getGuess: function() {
+        console.log("Enter a guess between '${smallestNum}' and '${biggestNum}'")
+    },
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     }
   }
+  
