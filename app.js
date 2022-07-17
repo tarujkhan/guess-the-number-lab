267ba5a31798d4986df12b9f1bbe5c036871dbf6
tarujkhan <tarujkhan@yahoo.com>
2022-07-17 17:53:42 -0500

    Just added some new logic and seeing some progress.

--- app.js
+++ app.js
@@ -3,24 +3,42 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    guessNum: null,
     prevGuess: [],
     getGuess: function() {
         guessNum = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
         return guessNum;
                    
     },
+
+    guessNum: null,
+
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-        this.secretNum;
-        let newguess = 0;
-        while(newguess > this.smallestNum.length && newguess < this.biggestNum.length);
-        newguess++; 
-        console.log(newguess); {
-          this.prevGuess.push(newguess);
-          return this.prevGuess;
-          console.log(this.prevGuess);
+        while (this.getGuess()!= this.secretNum) { // the loop is going to run if they are not equal
+          if(guessNum != 0) { 
+            this.render(); // the value of getguess will be put into guessnum and if guessnum is not equal to 0 render will be called
+          }
         }
+      },
+
+      render: function () {
+        if(guessNum > this.secretNum) {
+          alert("Too high")
+        } else {
+          alert("Too low")
+        }
+      }
+        // this.secretNum;
+        // let newguess = 0;
+        // while(newguess > this.smallestNum.length && newguess < this.biggestNum.length);
+        // newguess++; 
+        // console.log(newguess); {
+        //   this.prevGuess.push(newguess);
+        //   return this.prevGuess;
+        //   console.log(this.prevGuess);
+        // }
     }
-  }
+  
   console.log(game.play())
